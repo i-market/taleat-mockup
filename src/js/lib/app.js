6d46b06612dcd26217bc79b4547b6b818da32e31
Artem Novikov <novikov@i-market.ru>
2017-10-26 18:35:51 +0300

    move .input-number jquery plugin out of the init function

--- src/js/lib/app.js
+++ src/js/lib/app.js
@@ -1,3 +1,56 @@
+// input number
+(function ($) {
+  "use strict";
+
+  function InputNumber(element) {
+    this.$el = $(element);
+    this.$input = this.$el.find('[type=text]');
+    this.$inc = this.$el.find('[data-increment]');
+    this.$dec = this.$el.find('[data-decrement]');
+    this.min = this.$el.attr('data-min') || false;
+    this.max = this.$el.attr('data-max') || false;
+    this.init();
+  }
+
+  InputNumber.prototype = {
+    init: function () {
+      this.$dec.on('click', $.proxy(this.decrement, this));
+      this.$inc.on('click', $.proxy(this.increment, this));
+    },
+
+    increment: function (e) {
+      var value = this.$input[0].value;
+      value++;
+      if (!this.max || value <= this.max) {
+        this.$input[0].value = value++;
+        this.$input.trigger('change');
+      }
+    },
+
+    decrement: function (e) {
+      var value = this.$input[0].value;
+      value--;
+      if (!this.min || value >= this.min) {
+        this.$input[0].value = value;
+        this.$input.trigger('change');
+      }
+    }
+  };
+
+  $.fn.inputNumber = function (option) {
+    return this.each(function () {
+      var $this = $(this),
+        data = $this.data('inputNumber');
+
+      if (!data) {
+        $this.data('inputNumber', (data = new InputNumber(this)));
+      }
+    });
+  };
+
+  $.fn.inputNumber.Constructor = InputNumber;
+})(jQuery);
+
 window.Mockup = {
   openModal: function($modal) {
     var duration = 150;
@@ -86,59 +139,6 @@ function onReady() {
     });
     $('[data-tabLinks]').parent().find('[data-tabLinks]').filter(':first-child').trigger('click');
   });
-  // input number
-  ;
-  (function ($) {
-    "use strict";
-
-    function InputNumber(element) {
-      this.$el = $(element);
-      this.$input = this.$el.find('[type=text]');
-      this.$inc = this.$el.find('[data-increment]');
-      this.$dec = this.$el.find('[data-decrement]');
-      this.min = this.$el.attr('data-min') || false;
-      this.max = this.$el.attr('data-max') || false;
-      this.init();
-    }
-
-    InputNumber.prototype = {
-      init: function () {
-        this.$dec.on('click', $.proxy(this.decrement, this));
-        this.$inc.on('click', $.proxy(this.increment, this));
-      },
-
-      increment: function (e) {
-        var value = this.$input[0].value;
-        value++;
-        if (!this.max || value <= this.max) {
-          this.$input[0].value = value++;
-          this.$input.trigger('change');
-        }
-      },
-
-      decrement: function (e) {
-        var value = this.$input[0].value;
-        value--;
-        if (!this.min || value >= this.min) {
-          this.$input[0].value = value;
-          this.$input.trigger('change');
-        }
-      }
-    };
-
-    $.fn.inputNumber = function (option) {
-      return this.each(function () {
-        var $this = $(this),
-          data = $this.data('inputNumber');
-
-        if (!data) {
-          $this.data('inputNumber', (data = new InputNumber(this)));
-        }
-      });
-    };
-
-    $.fn.inputNumber.Constructor = InputNumber;
-  })(jQuery);
   $('.input-number').inputNumber();
   // отзывы
   $('.reviews-item-link').on('click', function () {
